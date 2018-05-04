af950978ca8fd8723ed2d18ebe978a9c18693480
YanC Lin <yonalin1@gmail.com>
2018-05-04 17:00:44 +0000

    orders_controller

---
+++ rails project/delivery/app/controllers/orders_controller.rb
@@ -0,0 +1,39 @@
+class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find_by(id: params[:id])
+    @cutomer = @order.cutomers.build(user_id:current_user.id)
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    order = Order.create(order_params)
+    redirect_to order_path(order)
+  end
+
+  def edit
+    @order = Order.find_by(id: params[:id])
+    @customer = @order.cutomers.build(user_id:current_user.id)
+  end
+
+  def update
+    order = Order.find_by(id: params[:id])
+    order.update(order_params)
+    redirect_to order_path(order)
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(
+        :name,
+        :address,
+        :food
+      )
+  end
+end
