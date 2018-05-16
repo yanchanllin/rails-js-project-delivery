23256a7bfd4ac465ba33747478e3b7491ec7dbf7
YanC Lin <yonalin1@gmail.com>
2018-05-16 20:15:19 +0000

    migrate

---
+++ rails project/delivery/db/migrate/20180516201342_create_orders.rb
@@ -0,0 +1,10 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.integer :user_id
+      t.integer :customer_id
+     t.datetime "created_at", null: false
+     t.datetime "updated_at", null: false
+    end
+  end
+end
