6dfe82a9d5ecec7614ae5df30908eba5cdba2f53
yanchanllin <yonalin1@gmail.com>
2018-05-23 13:46:07 -0400

    customers_controller

--- rails project/delivery/app/controllers/cutomers_controller.rb
+++ rails project/delivery/app/controllers/cutomers_controller.rb
@@ -1,9 +1,40 @@
 class CustomersController < ApplicationController
-  def create
-    customer = Customer.create(customer_params)
-    message = customer.customer_info
-    redirect_to user_path(customer.user), flash: { message:message }
+  
+  def index
+    @customers = Customer.all
   end
+
+  def show
+    @customer = Customer.find_by(id: params[:id])
+    @cutomer = @customer.orders.build(user_id:current_user.id)
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  
+  def create
+    if (customer = Customer.create customer_params)
+      session[:customer_id] = customer.id
+      redirect_to customer_path(customer)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @customer = Customer.find_by(id: params[:id])
+    @customer = @customer.orders.build(user_id:current_user.id)
+  end
+
+  def update
+    customer = Customer.find_by(id: params[:id])
+    customer.update(customer_params)
+    redirect_to customer_path(customer)
+  end
+  
+  
 
   private
   def customer_params
