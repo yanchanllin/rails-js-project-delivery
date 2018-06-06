b15c4274670a0c3f05a7db7365d59374ecbc613a
yanchanlin <yonalin1@gmail.com>
2018-06-06 11:27:56 -0400

    meal 2

--- rails project/delivery/config/routes.rb
+++ rails project/delivery/config/routes.rb
@@ -1,29 +1,28 @@
 Rails.application.routes.draw do
-  get 'meal/name:string'
   get '/users/new', to: 'users#new', as: 'new_user'
  root 'static_pages#home'
  get '/users', to:'users#index', as: 'users'
  post '/users', to:'users#create'
  get '/users/:id', to: 'users#show', as: 'user'
  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
- get '/customers', to: 'customers#index', as: 'customers'
+ get '/meals', to: 'meals#index', as: 'meals'
  get '/signin', to: 'session#new', as: 'signin'
  post '/session', to: 'session#create', as: 'session'
  delete '/session/', to: 'session#destroy'
- get '/customers/new', to: 'customers#new', as: 'new_customer'
- get '/customers/:id', to: 'customers#show', as: 'customer'
- get '/customers/:id/edit', to: 'customers#edit', as: 'edit_customer'
- patch '/customers/:id', to: 'customers#update'
- post '/customers', to: 'customers#create'
+ get '/meals/new', to: 'meals#new', as: 'new_meal'
+ get '/meals/:id', to: 'meals#show', as: 'meal'
+ get '/meals/:id/edit', to: 'meals#edit', as: 'edit_meal'
+ patch '/meals/:id', to: 'meals#update'
+ post '/meals', to: 'meals#create'
  get '/orders/new', to: 'orders#new', as: 'new_order'
  post '/orders', to:"orders#create", as: 'orders'
  get '/orders/:id', to:"orders#show", as: 'order'
  get '/orders/:id/edit', to: 'orders#edit', as: 'edit_order'
- #get 'customers/:customer_id/orders/new', to: 'orders#new', as: 'new_customer_order'
- #post '/customers/:id/orders', to: 'orders#create'
+ #get 'meals/:meal_id/orders/new', to: 'orders#new', as: 'new_meal_order'
+ #post '/meals/:id/orders', to: 'orders#create'
  patch '/orders/:id', to: 'orders#update'
 
- resources :customers do
+ resources :meals do
   resources :orders 
 
  end
