86df3589c5f443671f507e7ea47bafd0d84839cc
yanchanlin <yonalin1@gmail.com>
2018-07-01 20:14:38 -0400

    session controller-create

--- rails project/delivery/app/controllers/session_controller.rb
+++ rails project/delivery/app/controllers/session_controller.rb
@@ -5,13 +5,22 @@ class SessionController < ApplicationController
     @user = User.new
   end
 
-def create  
-  if auth_hash= request.env['omniauth.auth']
-    raise auth_hash.inspect 
+def create 
+  if request.env["omniauth.auth"]
+   @user = User.find_or_create_by(uid: auth['uid']) do |u|
+    u.name = auth['info']['name']
+    u.email = auth['info']['email']
+    u.image = auth['info']['image']
+    session[:user_id] = @user.id
+
+    redirect_to user_path(@user)
+  end    
   else 
     @user = User.find_by(name:params[:user][:name])
     if @user && @user.authenticate(params[:password])
+     
     session[:user_id] = @user.id
+
     redirect_to user_path(@user)
     else 
       render 'sessions/new'
@@ -19,8 +28,9 @@ def create
   end 
 end
 
+
 def destroy
-    session.delete("user_id")
+   session.delete("user_id")
   redirect_to root_path
 end 
 
