3d06442fea206b977d5e6b8a668fe847c1533d82
yanchanlin <yonalin1@gmail.com>
2018-07-10 17:51:57 -0400

    clean up/scope23

--- rails project/delivery/app/models/order.rb
+++ rails project/delivery/app/models/order.rb
@@ -2,10 +2,10 @@ class Order < ActiveRecord::Base
   belongs_to :meal
   belongs_to :user
   validates :quantity, presence: true
-  scope :most_recent, -> {where(most_recent: true)}
-  
+  scope :most_recent, -> {where{order(created_at: :DESC).limit(1)}}
+
   def self.most_recent
-    where(most_recent: true)
+    where{order(created_at: :DESC).limit(1)}
   end
 
   
