fe27457ccb5c88f5072b57c28c2aa6aa230726dd
yanchanllin <yonalin1@gmail.com>
2018-11-02 10:33:35 -0700

    updated meal model next

--- rails project/delivery/app/models/meal.rb
+++ rails project/delivery/app/models/meal.rb
@@ -7,5 +7,16 @@ class Meal < ApplicationRecord
   def self.most_ordered
     Order.joins(:meal).group('meals.id').order('count(meals.id)DESC').limit(1).first
   end 
+
+  def next
+    meal = Meal.where("id > ?", id).first
+
+    if meal
+      meal
+    else
+      Meal.first
+    end
+
+  end
 end
 
