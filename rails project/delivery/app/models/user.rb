650bfab45b618cf07840fe5c9682a71be3bc964c
yanchanllin <yonalin1@gmail.com>
2018-11-02 10:41:21 -0700

    updated user model self.from_omniauth

--- rails project/delivery/app/models/user.rb
+++ rails project/delivery/app/models/user.rb
@@ -5,4 +5,17 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :password, length: { in:1...30 } 
 
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.password = SecureRandom.hex
+      user.oauth_token = auth.credentials.token
+      user.oath_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+
+
 end
