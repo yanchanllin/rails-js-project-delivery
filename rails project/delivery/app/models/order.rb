f451270e4cf0a1541aec6eef7051b908ed595f0a
yanchanllin <yonalin1@gmail.com>
2018-11-02 10:37:49 -0700

    updated order model next_order

--- rails project/delivery/app/models/order.rb
+++ rails project/delivery/app/models/order.rb
@@ -3,6 +3,14 @@ class Order < ActiveRecord::Base
   belongs_to :user
   validates :quantity, presence: true
   
+  def next_order
+    next_order = Order.where(meal_id: meal).where("id > ?", id).first
 
+    if next_order
+      next_order
+    else
+      Order.where(meal_id: meal).first
+    end
+  end
   
  end
