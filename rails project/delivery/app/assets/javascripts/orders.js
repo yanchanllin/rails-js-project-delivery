2e744c5588b57b134f1960302eb4f8deacfe7ece
yanchanllin <yonalin1@gmail.com>
2018-11-02 08:52:53 -0700

    added asset js orders.js orderClickHandlers

--- rails project/delivery/app/assets/javascripts/orders.js
+++ rails project/delivery/app/assets/javascripts/orders.js
@@ -3,3 +3,14 @@ $(() => {
     seeOrders();
     submitOrder();
   })
+
+  const orderClickHandlers = () => {
+    $(document).on("click", ".js-nextOrder", function(event) {
+      event.preventDefault();
+      let orderId = $(this).attr('data-id')
+      let mealId = $('.mealOrders').attr("data-id")
+      let href = $(this).attr("href")
+      mealName(href);
+      seeNextOrder(mealId, orderId);
+    })
+  }
