bb1a8838ab3638c3069b61b02bd308b58ea76f36
yanchanllin <yonalin1@gmail.com>
2018-11-06 09:06:12 -0800

    create comments in db

--- rails project/delivery/db/schema.rb
+++ rails project/delivery/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_16_214706) do
+ActiveRecord::Schema.define(version: 2018_11_06_170004) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "meals", force: :cascade do |t|
     t.string "name"
