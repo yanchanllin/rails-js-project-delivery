6ba0c31d1b1d4856d2dceaaaee8d94cb710397ec
yanchanllin <yonalin1@gmail.com>
2018-11-06 09:23:14 -0800

    updated index,show,new and create in orders controller

--- rails project/delivery/app/controllers/orders_controller.rb
+++ rails project/delivery/app/controllers/orders_controller.rb
@@ -2,95 +2,43 @@ class OrdersController < ApplicationController
   
 
  def index
-    @meal = Meal.find(params[:meal_id])
-    @orders = @meal.orders 
+    @orders = Order.all 
     respond_to do |f|
-      f.html {render :index}
-      f.json {render json: @meal, include: :orders}
+      f.html
+      f.json {render json: @orders}
     end 
   end
   
   def show
-    @order = Order.find_by(id: params[:id])
     respond_to do |f|
-      f.html {render @order}
+      f.html 
       f.json {render json: @order}
     end 
   end
 
   def new 
-    @meal = Meal.find(params[:meal_id])
     @order = Order.new
-    render template: 'orders/new'
   end
 
   def create
-    if !params[:meal_id]
-    @order = Order.new(order_params)
-    @order.user_id = current_user.id
-    if @order.save
-      respond_to do |f|
-        f.html {redirect_to meal_orders_path(@order.meal) }
-        f.json {render json: @order}
-      end
+    @current_user = Meal.find(session[:meal_id]) if session[:meal_id]
+    if @current_user.orders.create(order_params).valid?
+      @current_user.orders.create(order_params)
+      flash[:notice] = 'Order was successfully created.'
+      redirect_to orders_path
+  
     else
-    redirect_to user_path(current_user), :notice => "can't be blank"
-  end
-else
-  @meal = Meal.find(params[:meal_id])
-  @order = @meal.orders.create(orders_params)
-  @order.user_id = current_user.id
-    if @order.save
-      respond_to do |f|
-        f.html {redirect_to meal_orders_path(@order.meal)}
-        f.json {render json: @order}
-      end
-    else
-       redirect_to new_meal_order_path, :notice => "can't be blank"
+      redirect_to new_order_path
     end
   end
-end
-
-def next_order
-  @order = Order.find(params[:id])
-  @next_order = @order.next_order
-  respond_to do |f|
-    f.html {render @next_order}
-    f.json {render json: @next_order.to_json(
-      { :include => [
-          user: {
-            only: [:username]
-          },
-          meal: {
-           only: [:name]
-          }
-        ]
-      }
-    )
-  }
-  end
-end
-
-def all_orders
-  @orders = Order.all
-  respond_to do |f|
-    f.html {render @orders}
-    f.json {render json: @orders.to_json(
-      { :include => [
-          user: {
-            only: [:username]
-          },
-          meal:{}
-        ]
-      }
-    )
-  }
-  end
-end
 
 def highest_ordered
   @orders = Order.all
 
+end
+
+def users_orders
+  @user = User.find_by_id(session[:user_id])
 end
 
   def edit
@@ -102,10 +50,6 @@ end
     order = Order.find_by(id: params[:id])
     order.update(order_params)
     redirect_to user_path(current_user)
-  end
-
-  def users_orders
-    @user = User.find_by_id(session[:user_id])
   end
 
   def destroy
